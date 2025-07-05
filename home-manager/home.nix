5b8c5131cab31a12e46905e3daaa7f4455d0217b
Kentaro Wuertele <ken@wuertele.com>
2025-07-05 07:59:20 -0700

    Adding obs.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -241,6 +241,15 @@
   };
 
   programs = {
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+      ];
+    };
+
     home-manager.enable = true;
     
     git = {
