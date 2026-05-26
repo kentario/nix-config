c87cad8f50fa49f879206dfbe42e1343f949db41
Kentaro Wuertele <ken@wuertele.com>
2026-05-26 11:29:10 -0700

    Attempting to fix tuigreet config.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,7 +60,7 @@
       enable = true;
       settings = {
         default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --asterisks --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland --asterisks --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red'";
 	user = "greeter";
         };
       };
